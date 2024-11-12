637298bcd3fdf7ccfc974d29247d6860b6036007
Falekin7 <falekinldg@gmail.com>
2024-11-11 22:14:18 -0300

    background stars and sun

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ const observer = new IntersectionObserver(
       }
     });
   },
-  { rootMargin: '-70% 0px -30% 0px' }
+  { rootMargin: '-60% 0px -40% 0px' }
 );
 
 menuLinks.forEach((menuLink) => {
@@ -37,3 +37,52 @@ menuLinks.forEach((menuLink) => {
     observer.observe(target);
   }
 });
+
+document
+  .getElementById('inicio-link')
+  .addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const target = document.getElementById('inicio');
+
+    target.scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+
+document
+  .getElementById('inicio-link')
+  .addEventListener('click', function (event) {
+    event.preventDefault(); // Prevenir el comportamiento por defecto del enlace
+
+    // Encuentra la sección de destino por ID
+    const target = document.getElementById('profile');
+
+    // Calcula la distancia desde la parte superior del documento hasta la sección de destino
+    const headerHeight = document.querySelector('header').offsetHeight; // Altura del header
+    const targetPosition = target.offsetTop - headerHeight; // Desplazamiento con el ajuste para el header
+
+    // Desplazamiento suave hacia la sección
+    window.scrollTo({
+      top: targetPosition,
+      behavior: 'smooth',
+    });
+  });
+
+// STARS EFFECTS //
+const starContainer = document.querySelector('.star-background');
+const numberOfStars = 200;
+
+for (let i = 0; i < numberOfStars; i++) {
+  const star = document.createElement('div');
+  star.classList.add('star');
+
+  // Posición aleatoria para cada estrella
+  star.style.width = `${Math.random() * 3}px`; // Tamaño aleatorio
+  star.style.height = star.style.width;
+  star.style.top = `${Math.random() * 100}vh`;
+  star.style.left = `${Math.random() * 100}vw`;
+  star.style.animationDelay = `${Math.random() * 2}s`; // Retraso aleatorio
+
+  starContainer.appendChild(star);
+}
